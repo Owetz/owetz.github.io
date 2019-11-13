7cde680ef53694d9e5f01ff44cd6c448e4b7dc15
Daniel Ahl <daniel.ahl@studentmedia.se>
2019-11-13 11:41:30 +0100

    Updates

---
+++ precache-manifest.c919789373dbdf3a430df50a0a631e7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64dcd352ff0db70e0fa5e9f2668a85e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f1eeddc8702c069ca50",
+    "url": "/static/css/main.73901849.chunk.css"
+  },
+  {
+    "revision": "ca92da6268aff099f463",
+    "url": "/static/js/2.1748879e.chunk.js"
+  },
+  {
+    "revision": "5f1eeddc8702c069ca50",
+    "url": "/static/js/main.35433c26.chunk.js"
+  },
+  {
+    "revision": "2393e70fb6c8aa85d429",
+    "url": "/static/js/runtime-main.17b8024a.js"
+  }
+]);
