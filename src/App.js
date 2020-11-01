533b1ca05a64e46337a98e06c3ed3be634bf8fc8
Daniel <danielahl89@gmail.com>
2020-11-01 15:45:52 +0100

    Ingen aning om vad detta är, men säkert bra ändringar.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import './App.css';
-import TopNavBar from './Components/TopNavBar/TopNavBar';
+import React from "react";
+import "./App.css";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faFacebookSquare, faTwitterSquare, faInstagramSquare, faLinkedin} from '@fortawesome/free-brands-svg-icons';
 
 function App() {
   return (
     <>
-      <TopNavBar />
-      <main className="landingpage">
-        <h1 className="title">Daniel Owetz Ahl</h1>
-        <h2 className="subtitle">Frontend Developer</h2>
-      </main>
+    <header>
+      <img src={require('./images/Owetz_logo.png')} className="logo" alt="Owetz Logotype"/>
+    </header>
+    <main className="header">
+      <p className="text">Hej, jag heter <span className="accent">Daniel Ahl</span>.</p>
+      <p class="text">Jag är en Webbutvecklare.</p>
+      <a className="cBtn" href="mailto:info@owetz.se">Kontakta mig</a>
+      <section className="socialSection">
+        <a href="https://www.facebook.com/Tvahlen" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faFacebookSquare} /></a>
+        <a href="https://twitter.com/danahlen" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faTwitterSquare} /></a>
+        <a href="https://www.instagram.com/danahlen/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faInstagramSquare} /></a>
+        <a href="https://www.linkedin.com/in/owetz/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a>
+      </section>
+    </main>
     </>
   );
 }
